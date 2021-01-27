d52c2f99d9300b22a059f2c89d415c2fe3f2a94a
xiaox <xiaoxingxing9426@gmail.com>
2021-01-27 15:21:55 +0800

    code

--- loginin.py
+++ loginin.py
@@ -7,22 +7,16 @@
 
 def login(n, m):
     dic = {"admin": "admin@123", "guess": "123456", "my": "my_666"}
-    if n or m not in dic:
-        return False
-    elif n.val != m or m.key != n:
-        return False
+    if dic[n] == m:
+        print("登录成功")
     else:
-        return True
+        print('您输入的账号或密码不正确')
 
 
 def main():
     inputs = input("请输入账号：")
     inputs1 = input("请输入密码：")
-    rec = login(inputs, inputs1)
-    if rec:
-        print("登录成功")
-    else:
-        print("您输入的账号或密码不正确")
+    login(inputs, inputs1)
 
 
 if __name__ == '__main__':
