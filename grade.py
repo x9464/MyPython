2973e7d5c093a7206fcc2fe9b8e01b26dddaf58c
xiaox <xiaoxingxing9426@gmail.com>
2021-02-09 13:00:01 +0800

    code

--- grade.py
+++ grade.py
@@ -6,9 +6,9 @@
 
 
 def main():
-    MyGrade = 361
+    MyGrade = 451
     Grade = 451
-    result = "通过一本录取分数线" if MyGrade > Grade else "未通过一本录取分数线"
+    result = "通过一本录取分数线" if MyGrade >= Grade else "未通过一本录取分数线"
     print(" 您的高考总分：", MyGrade, "\n", "第一批本科录取分数线: ", Grade, "\n", "高考结果：", result)
 
 
